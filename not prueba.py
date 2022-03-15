cbcb70e7fce001933754afd0520a07af7bd0c7a2
Penfieldmoodorgan <101405415+Penfieldmoodorgan@users.noreply.github.com>
2022-03-15 08:07:37 +0400

    Update not prueba.py

--- not prueba.py
+++ not prueba.py
@@ -6,4 +6,4 @@ Q = {('x','x'):-1, ('x','z'):2, ('z','x'):0, ('z','z'):-1}
 sampleset = sampler.sample_qubo(Q, num_reads=6000)
 print(sampleset)
 
-sampleset.first.sample["x"] =! sampleset.first.sample["z"]
+sampleset.first.sample["x"] != sampleset.first.sample["z"]
