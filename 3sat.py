a60afed8abec8398b86bd7fa674ae30198be3f1d
Penfieldmoodorgan <101405415+Penfieldmoodorgan@users.noreply.github.com>
2022-03-16 16:57:50 +0400

    Update 3sat.py

--- 3sat.py
+++ 3sat.py
@@ -1,10 +1,13 @@
 from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import EmbeddingComposite
+import dwave.inspector
 sampler = EmbeddingComposite(DWaveSampler(solver={'topology__type': 'chimera'}))
 
 linear = {('x','x'):-1, ('z','z'):-1, ('y','y'):-1}
 quadratic = {('y','z'):2, ('x','y'):2, ('x','z'):2 }
 Q = {**linear,**quadratic}
 
-sampleset = sampler.sample_qubo(Q, num_reads=6000)
+sampleset = sampler.sample_qubo(Q, num_reads=1000)
 print(sampleset)
+
+dwave.inspector.show(sampleset)
