cc31ba3d3dcae66d38f73635d2d197496838f672
Penfieldmoodorgan <101405415+Penfieldmoodorgan@users.noreply.github.com>
2022-03-17 09:31:42 +0400

    Update 3sat.py

--- 3sat.py
+++ 3sat.py
@@ -1,13 +1,27 @@
 from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import EmbeddingComposite
+from dimod import BinaryQuadraticModel
 import dwave.inspector
-sampler = EmbeddingComposite(DWaveSampler(solver={'topology__type': 'chimera'}))
+
+# Define problem QUBO as dictionary
 
 linear = {('x','x'):-1, ('z','z'):-1, ('y','y'):-1}
 quadratic = {('y','z'):2, ('x','y'):2, ('x','z'):2 }
 Q = {**linear,**quadratic}
 
-sampleset = sampler.sample_qubo(Q, num_reads=1000)
+# Convert to BQM to submit to QPU
+
+bqm=BinaryQuadraticModel.from_qubo(Q)
+
+# Define sampler, auto minor-embedding 
+
+sampler = EmbeddingComposite(DWaveSampler())
+
+# Run the problem and print the results
+
+sampleset = sampler.sample(bqm, num_reads=1000)
 print(sampleset)
+
+# Show problem details and solver details
 
 dwave.inspector.show(sampleset)
